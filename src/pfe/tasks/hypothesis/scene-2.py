f9cd557a481f0d046a028bae52e5d2b12c3ca784
konstantin-ogulchansky <k.ohulchanskyi@gmail.com>
2020-12-06 02:42:21 +0100

    Updated `scene-2.py`.

--- src/pfe/tasks/hypothesis/scene-2.py
+++ src/pfe/tasks/hypothesis/scene-2.py
@@ -2,56 +2,13 @@
 Test the hypothesis.
 """
 
-import random
-import sys
-from collections import Counter
-from typing import Iterable, Callable
-
 import powerlaw as pl
 import scipy.stats as st
 
-from pfe.misc.log import timestamped, nothing, cx
+from pfe.misc.log import timestamped, cx
 from pfe.misc.plot import Plot
 from pfe.parse import parse, publications_in
-from pfe.tasks.hypothesis import degree_distribution
-from pfe.tasks.statistics import Statistic
-
-
-def sample(cdf: dict[int, float], size: int, log: Callable = nothing) -> Iterable[int]:
-    """Draws a sample of the provided `size` according to `cdf`.
-
-    This function implements the inverse transform sampling for drawing a sample
-    according to an arbitrary distribution, defined by the provided CDF.
-
-    .. [1] Wikipedia,
-       https://en.wikipedia.org/wiki/Inverse_transform_sampling
-
-       [2] Inverse Transform Sampling,
-       https://stephens999.github.io/fiveMinuteStats/inverse_transform_sampling.html
-    """
-
-    x = list(sorted(cdf.keys()))
-
-    def draw(i: int) -> int:
-        u = random.uniform(0, 1)
-        p = 0
-
-        log(f'Sampling {i}/{size} with `u` = {u}.')
-
-        for i in range(len(x)):
-            q = cdf[x[i]]
-
-            if p <= u < q:
-                return x[i]
-            else:
-                p = q
-
-        log(f'The value for `u` = {u} was not found; returning {(m := max(x))}.',
-            file=sys.stderr)
-
-        return m
-
-    return (draw(i) for i in range(size))
+from pfe.tasks.hypothesis import degree_distribution, chi_squared
 
 
 if __name__ == '__main__':
@@ -59,7 +16,7 @@ if __name__ == '__main__':
     log('Starting...')
 
     # Construct a graph.
-    graph = parse(publications_in('COMP', between=(1990, 2018), log=log))
+    graph = parse(publications_in('COMP', between=(1990, 1996), log=log))
 
     log(f'Read a graph with '
         f'{graph.number_of_nodes()} nodes and '
@@ -73,42 +30,50 @@ if __name__ == '__main__':
     with cx(log, 'Computing the fit...'):
         fit = pl.Fit(list(statistic.sequence()), discrete=True)
 
-    # Compute the "truncated" statistic (within (`x_min`, `x_max`)).
+    # Compute the "truncated" statistic (within [`x_min`, `x_max`]).
     x_min = fit.xmin or min(statistic.keys())
     x_max = fit.xmax or max(statistic.keys())
 
     if not isinstance(x_min, int):
         assert x_min.is_integer(), 'Strangely enough, `x_min` is not an integer.'
         x_min = round(x_min)
+
     if not isinstance(x_max, int):
         assert x_max.is_integer(), 'Strangely enough, `x_max` is not an integer.'
         x_max = round(x_max)
 
     truncated = statistic.truncate(x_min, x_max)
 
-    # Generate a sample.
-    with cx(log, 'Generating a sample...'):
-        x = list(range(x_min, 3 * x_max + 1))
-        y = fit.truncated_power_law.cdf(x)
+    x = list(sorted(truncated.keys()))
 
-        assert len(x) == len(y), f'`x` and `y` must have the same size.'
+    cdf = dict(zip(x, fit.truncated_power_law.cdf(x)))
+    pdf = dict(zip(x, fit.truncated_power_law.pdf(x)))
 
-        cdf = dict(zip(x, y))
+    assert abs(1 - sum(pdf.values())) <= 1e-3, '`sum p_i` must equal 1.'
+    assert pdf.keys() == truncated.keys()
 
-        sampled = sample(cdf, size=100000)
-        sampled = Statistic(Counter(sampled)).cdf()
+    # Plot PDF.
+    plot = Plot(title='PDF')
+    plot.scatter(truncated.normalized())
+    plot.scatter(pdf, style={'color': 'red'})
 
-    # Plot.
-    plot = Plot(log=log)
+    fit.truncated_power_law.plot_pdf(ax=plot.ax)
 
-    plot.scatter(truncated.cdf())
+    plot.show()
+
+    # Plot CDF.
+    plot = Plot(title='CDF')
     plot.scatter(cdf, style={'color': 'red'})
-    plot.scatter(sampled, style={'color': 'green'})
 
     fit.truncated_power_law.plot_cdf(ax=plot.ax)
 
     plot.show()
 
-    # Perform a K-S test.
-    with cx(log, 'Performing K-S test...'):
-        test = st.kstest(list(statistic.sequence()), sampled)
+    # Calculate the statistic.
+    n = truncated.total()
+
+    obs = [truncated[x] for x in sorted(truncated.keys())]
+    exp = [pdf[x] * n for x in sorted(pdf.keys())]
+
+    print(chi_squared(truncated, pdf))
+    print(st.chisquare(obs, exp))
