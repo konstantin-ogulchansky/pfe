97160e3956cf4e69ca654576d6da0ac799764352
konstantin-ogulchansky <k.ohulchanskyi@gmail.com>
2020-12-12 09:58:32 +0100

    Added TODOs to `model.py`.

--- src/pfe/model.py
+++ src/pfe/model.py
@@ -49,9 +49,14 @@ class Parameters:
             raise ValueError('`n0` must be less or equal to `n`.')
 
         if not 0 <= self.pv <= 1:
-            raise ValueError('`pv` must be in [0, 1].')
+            raise ValueError('`pv` must be in the range [0, 1].')
         if not 0 <= self.pve <= 1:
-            raise ValueError('`pve` must be in [0, 1].')
+            raise ValueError('`pve` must be in the range [0, 1].')
+
+        # TODO: Ask.
+        # Is this a correct check to add?
+        if self.pv + self.pve > 1:
+            raise ValueError('`pv + pve` must be less than 1.')
 
         if len(set(self.p.shape)) != 1 or len(self.p.shape) != self.p.shape[0]:
             raise ValueError('`p` has an incorrect form.')
@@ -79,6 +84,7 @@ class Graph:
         self.nodes = nodes
         self.edges = edges
 
+        # TODO: Ask.
         # What are these?
         self.e = e
         self.v = v  # Why do we need it? It doesn't seem to be used when generating a graph.
@@ -141,6 +147,10 @@ class Graph:
 
     def add_node_and_edge(self, parameters: Parameters):
         """Adds a node and an edge to the graph. Or does it?"""
+
+        # TODO: Ask.
+        # Should there be an implementation? :)
+        # Because currently we cannot specify `pve` other than 0.
         raise ಠ_ಠ("This shouldn't have happened...")
 
     def add_edge(self, parameters: Parameters):
@@ -175,6 +185,13 @@ class Graph:
         e1 = list(hyperedge(q1, h1))
         e2 = list(hyperedge(q2, h2))
 
+        # TODO: Ask.
+        # Is it okay that `e1` and `e2` may contain duplicate nodes?
+        # For example,, we can obtain a hyperedge like [1, 2, 1, 3].
+        # This incorrectly affects the lists `v`, `d` and `e`, since the
+        # duplicate node will be updated twice in the following loops.
+        # For example, the degree of a node 1 will be updated twice,
+        # even though we add only a single hyperedge.
         self.edges.append(e1 + e2)
 
         for u in e1:
@@ -213,6 +230,11 @@ if __name__ == '__main__':
             p=[[0.25, 0.25], [0.25, 0.25]],
             m=[0.5, 0.5],
             gamma=20,
+
+            # TODO: Ask.
+            # Shouldn't it be with `loc = 1` to generate a regular graph?
+            # In the model, we concatenate two hyperedges of sizes 2,
+            # which results in adding a hyperedge of size 4.
             distribution=normal(loc=2, scale=0),
         )
 
