71aea58d18b77423bb9c5f0f1ab4d022b9e62f47
SofiiaShelest <soniashelest@gmail.com>
2021-04-08 10:44:58 +0200

    Update by_community_size.py

--- src/pfe/matrices/by_community_size.py
+++ src/pfe/matrices/by_community_size.py
@@ -7,8 +7,7 @@ from pfe.misc.log import Log, Pretty
 log: Log = Pretty()
 log.info('Starting...')
 
-data = Path('../../../data/graph/ig')
-test_stuff = Path('test-data')
+test_stuff = Path('test-data/COMP-data')
 
 
 def plot_cumulative_community_sizes(percent):
@@ -22,7 +21,7 @@ def plot_cumulative_community_sizes(percent):
 
     def find_index_of(l, val):
         for i in range(len(l)):
-            if abs(l[i] - val) <= .003:
+            if abs(l[i] - val) <= .005:
                 print(i)
                 return i
 
@@ -61,42 +60,46 @@ def plot_matrix_restricted_by_n_members(n_members):
     m = exclude_columns(m, odd_communities)
     m = fill_diagonal(m)
 
-    k = prob_matrix(m)
+    k = prob_matrix_by_row(m)
+    l = prob_matrix_by_all_publications(m)
 
     title = 'Probability matrix'
     subtitle = f'Communities with {n_members} or more'
 
     if 'sizes' in k.columns.tolist():
-        plot_matrix(k, f'matrix_restricted_by_n_members_{n_members}_prob', title, subtitle, prob=True,
+        plot_matrix(k, f'matrix_restricted_by_n_members_{n_members}_prob_by_row', title, subtitle, prob=True,
+                    exclude_columns=True)
+        plot_matrix(l, f'matrix_restricted_by_n_members_{n_members}_prob_by_upper_triangular', title, subtitle, prob=True,
                     exclude_columns=True)
     else:
-        plot_matrix(k, f'matrix_restricted_by_n_members_{n_members}_prob', title, subtitle, prob=True)
+        plot_matrix(k, f'matrix_restricted_by_n_members_{n_members}_prob_by_row', title, subtitle, prob=True)
+        plot_matrix(l, f'matrix_restricted_by_n_members_{n_members}_prob_by_upper_triangular', title, subtitle, prob=True)
 
 
 def plot_matrix_restricted_by_n_largest_communities(n_largest):
     largest_communities = get_n_largest_communities(n_largest)
-    print(largest_communities)
-    print(largest_communities)
-    print(largest_communities)
-    n_communities = number_of_communities()
 
     m = matrix()
-    m = pd.DataFrame(m, [str(i) for i in range(n_communities)], [str(i) for i in range(n_communities)])
+    m = to_dataframe(m)
     m = add_row_with_community_size(m)
 
     m = keep_columns(m, largest_communities)
     m = fill_diagonal(m)
 
-    k = prob_matrix(m)
+    k = prob_matrix_by_row(m)
+    l = prob_matrix_by_all_publications(m)
 
     title = 'Probability matrix'
     subtitle = f'{n_largest} largest communities'
 
     if 'sizes' in k.columns.tolist():
-        plot_matrix(k, f'matrix_restricted_by_n_largest_communities_{n_largest}_prob', title, subtitle, prob=True,
+        plot_matrix(k, f'matrix_restricted_by_n_largest_communities_{n_largest}_prob_by_row', title, subtitle, prob=True,
+                    exclude_columns=True)
+        plot_matrix(l, f'matrix_restricted_by_n_largest_communities_{n_largest}_prob_by_upper_triangular', title, subtitle, prob=True,
                     exclude_columns=True)
     else:
-        plot_matrix(k, f'matrix_restricted_by_n_largest_communities_{n_largest}_prob', title, subtitle, prob=True)
+        plot_matrix(k, f'matrix_restricted_by_n_largest_communities_{n_largest}_prob_by_row', title, subtitle, prob=True)
+        plot_matrix(l, f'matrix_restricted_by_n_largest_communities_{n_largest}_prob_by_upper_triangular', title, subtitle, prob=True)
 
 
 def get_communities_with_more_than_n_members(n_members):
@@ -119,10 +122,12 @@ def get_n_largest_communities(n):
     return [get_key(x) for x in y]
 
 
-plot_matrix_restricted_by_n_largest_communities(32)
-plot_matrix_restricted_by_n_largest_communities(42)
-plot_matrix_restricted_by_n_largest_communities(50)
+if __name__ == '__main__':
 
-plot_matrix_restricted_by_n_members(1000)
-plot_matrix_restricted_by_n_members(1500)
-plot_matrix_restricted_by_n_members(4000)
+    # plot_cumulative_community_sizes(0.8)
+    # plot_cumulative_community_sizes(0.9)
+    # plot_cumulative_community_sizes(0.95)
+
+    plot_matrix_restricted_by_n_largest_communities(18)
+    plot_matrix_restricted_by_n_largest_communities(24)
+    plot_matrix_restricted_by_n_largest_communities(28)
