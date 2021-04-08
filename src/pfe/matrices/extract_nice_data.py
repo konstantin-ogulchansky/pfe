d3e61cbd5c264704bcbb5d84f778cabcdfa7eaf2
SofiiaShelest <soniashelest@gmail.com>
2021-04-08 10:45:28 +0200

    Added extract_nice_data.py

---
+++ src/pfe/matrices/extract_nice_data.py
@@ -0,0 +1,54 @@
+
+# TODO: extract publications where at least one author is from Nice
+
+from pathlib import Path
+from typing import Tuple, Any, Iterable
+
+from pfe.preprocessing.clean import load, save
+
+
+def publications_in(*domains: str,
+                    between: Tuple[int, int],
+                    **kwargs: Any) -> Iterable[dict]:
+
+    def appropriate(publication: dict) -> bool:
+        if len(publication['authors']) >= 100:
+            return False
+
+        for author in publication['authors']:
+            if author['affiliation_city'] == 'Nice':
+                return True
+
+        return False
+
+    directory = Path('../../..')
+    new_directory = Path('test-data/COMP')
+
+    # Find the root of the repository.
+    while all(x.name != '.gitignore' for x in directory.iterdir()):
+        directory = directory.parent
+
+    files = [directory / 'data' / 'clean' / domain / f'{domain}-{year}.json'
+             for domain in domains
+             for year in range(between[0], between[1] + 1)]
+
+    for file in files:
+        publications = load(from_=file)
+
+        to_save = []
+        for publication in publications:
+            if appropriate(publication):
+                to_save.append(publication)
+
+        save(to_save, to=new_directory/file.name)
+
+
+if __name__ == '__main__':
+    from pfe.misc.log import Pretty
+
+    log = Pretty()
+    log.info('Starting.')
+
+    with log.scope.info(f'Saving'):
+        nice = publications_in('COMP', between=(1990, 2018), log=log)
+
