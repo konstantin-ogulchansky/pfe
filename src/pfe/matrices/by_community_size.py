86e7b2e6b5a90d73838843851b4dbed127620695
SofiiaShelest <soniashelest@gmail.com>
2021-04-12 14:37:43 +0200

    Update by_community_size.py

--- src/pfe/matrices/by_community_size.py
+++ src/pfe/matrices/by_community_size.py
@@ -10,8 +10,8 @@ log.info('Starting...')
 test_stuff = Path('test-data/COMP-data')
 
 
-def plot_cumulative_community_sizes(percent):
-    community_size = community_sizes()
+def plot_cumulative_community_sizes(percent, louvain=False):
+    community_size = community_sizes(louvain=louvain)
 
     y = list(sorted(community_size.values(), reverse=True))
     total = sum(y)
@@ -21,7 +21,7 @@ def plot_cumulative_community_sizes(percent):
 
     def find_index_of(l, val):
         for i in range(len(l)):
-            if abs(l[i] - val) <= .005:
+            if abs(l[i] - val) <= .006:
                 print(i)
                 return i
 
@@ -33,7 +33,7 @@ def plot_cumulative_community_sizes(percent):
     plt.axhline(y=percent)
     plt.axvline(x=find_index_of(x, percent))
 
-    fig.suptitle('Distribution of community sizes')
+    fig.suptitle(f'Distribution of community sizes ({ "Louvain" if louvain else "Leiden"})')
     ax.set_title(f'To get {percent * 100}% of the network about {find_index_of(x, percent)} largest communities needed')
     plt.xlabel('number of communities')
     plt.ylabel('part of nodes of the network')
@@ -49,13 +49,13 @@ def plot_cumulative_community_sizes(percent):
     # plt.show()
 
 
-def plot_matrix_restricted_by_n_members(n_members):
+def plot_matrix_restricted_by_n_members(n_members, louvain=False):
     odd_communities = get_communities_with_more_than_n_members(n_members)
-    n_communities = number_of_communities()
+    n_communities = number_of_communities(louvain=louvain)
 
-    m = matrix()
-    m = pd.DataFrame(m, [str(i) for i in range(n_communities)], [str(i) for i in range(n_communities)])
-    m = add_row_with_community_size(m)
+    m = matrix(louvain)
+    m = to_dataframe(m, louvain)
+    m = add_row_with_community_size(m, louvain)
 
     m = exclude_columns(m, odd_communities)
     m = fill_diagonal(m)
@@ -63,7 +63,7 @@ def plot_matrix_restricted_by_n_members(n_members):
     k = prob_matrix_by_row(m)
     l = prob_matrix_by_all_publications(m)
 
-    title = 'Probability matrix'
+    title = f'Probability matrix ( {"Louvain" if louvain else "Leiden"})'
     subtitle = f'Communities with {n_members} or more'
 
     if 'sizes' in k.columns.tolist():
@@ -76,12 +76,12 @@ def plot_matrix_restricted_by_n_members(n_members):
         plot_matrix(l, f'matrix_restricted_by_n_members_{n_members}_prob_by_upper_triangular', title, subtitle, prob=True)
 
 
-def plot_matrix_restricted_by_n_largest_communities(n_largest):
-    largest_communities = get_n_largest_communities(n_largest)
+def plot_matrix_restricted_by_n_largest_communities(n_largest, louvain=False):
+    largest_communities = get_n_largest_communities(n_largest, louvain=louvain)
 
-    m = matrix()
-    m = to_dataframe(m)
-    m = add_row_with_community_size(m)
+    m = matrix(louvain)
+    m = to_dataframe(m, louvain)
+    m = add_row_with_community_size(m, louvain)
 
     m = keep_columns(m, largest_communities)
     m = fill_diagonal(m)
@@ -102,15 +102,15 @@ def plot_matrix_restricted_by_n_largest_communities(n_largest):
         plot_matrix(l, f'matrix_restricted_by_n_largest_communities_{n_largest}_prob_by_upper_triangular', title, subtitle, prob=True)
 
 
-def get_communities_with_more_than_n_members(n_members):
-    c_sizes = community_sizes()
+def get_communities_with_more_than_n_members(n_members, louvain=False):
+    c_sizes = community_sizes(louvain=louvain)
     odd_communities = [i for i in c_sizes.keys() if c_sizes[i] < n_members]
 
     return odd_communities
 
 
-def get_n_largest_communities(n):
-    community_size = community_sizes()
+def get_n_largest_communities(n, louvain=False):
+    community_size = community_sizes(louvain=louvain)
     y = list(sorted(community_size.values(), reverse=True))
     y = y[:n]
 
@@ -124,10 +124,16 @@ def get_n_largest_communities(n):
 
 if __name__ == '__main__':
 
+    plot_cumulative_community_sizes(0.8, True)
+    plot_cumulative_community_sizes(0.9, True)
+    plot_cumulative_community_sizes(0.95, True)
     # plot_cumulative_community_sizes(0.8)
     # plot_cumulative_community_sizes(0.9)
     # plot_cumulative_community_sizes(0.95)
-
-    plot_matrix_restricted_by_n_largest_communities(18)
-    plot_matrix_restricted_by_n_largest_communities(24)
-    plot_matrix_restricted_by_n_largest_communities(28)
+    #
+    # plot_matrix_restricted_by_n_largest_communities(65)
+    # plot_matrix_restricted_by_n_largest_communities(82)
+    # plot_matrix_restricted_by_n_largest_communities(94)
+    # plot_matrix_restricted_by_n_largest_communities(28, True)
+    # plot_matrix_restricted_by_n_largest_communities(62, True)
+    # plot_matrix_restricted_by_n_largest_communities(162, True)
