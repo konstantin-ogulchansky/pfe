6398bbca623f7ee2aa21888352e4d6c288a13c12
SofiiaShelest <soniashelest@gmail.com>
2021-04-29 00:58:02 +0200

    Update generate_data_louvain.py

--- src/pfe/matrices/generate_data_louvain.py
+++ src/pfe/matrices/generate_data_louvain.py
@@ -12,24 +12,8 @@ from pfe.tasks.communities import louvain
 # new_data = Path('test-data/COMP-data')
 
 
-def create_data_louvain(graph: nx.Graph, mapping_path: Path,  new_data: Path):
+def create_data_louvain_relabeled_graph(graph: nx.Graph, mapping_path: Path,  new_data: Path):
     log: Log = Pretty()
-
-
-    # with log.scope.info('Reading `nx.Graph`.'):
-    #     graph = nx.read_weighted_edgelist(data / 'nx_comp_graph_relabeled_nodes.txt')
-    #     # graph = nx.read_edgelist(data / 'nx_comp_graph_relabeled_nodes.txt', data=False)
-    #
-    #     log.info(f'Read a graph with '
-    #              f'{blue | graph.number_of_nodes()} nodes and '
-    #              f'{blue | graph.number_of_edges()} edges.')
-    #
-    #     largest_component = max(nx.connected_components(graph), key=len)
-    #     graph = nx.subgraph(graph, largest_component)
-    #
-    #     log.info(f'Subgraph of largest connected component '
-    #              f'{blue | graph.number_of_nodes()} nodes and '
-    #              f'{blue | graph.number_of_edges()} edges.')
 
     louvain(graph, new_data, log)
 
@@ -111,3 +95,87 @@ def create_data_louvain(graph: nx.Graph, mapping_path: Path,  new_data: Path):
         with open(new_data / 'stats_largest_cluster.json', 'w') as file:
             json.dump(stats, file)
 
+
+
+
+
+def create_data_louvain(graph: nx.Graph, new_data: Path, publications_till=2018, log_file='', log=Pretty()):
+    louvain(graph, new_data, log)
+
+    with log.scope.info('Reading communities.'):
+        with open(new_data / 'louvain_communities.json', 'r') as file:
+            communities = json.load(file)
+
+    new_dict = {}
+
+    for k_community, v_authors in communities.items():
+        for author in v_authors:
+            new_dict[str(author)] = int(k_community)
+
+    communities = new_dict
+
+    with log.scope.info('Saving author-community into a file...'):
+        with open(new_data / 'louvain_author-community.json', 'w') as file:
+            json.dump(communities, file)
+
+    mapping = {}
+    with open(mapping_path, 'r') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            mapping[row[0]] = int(row[1])
+
+    publication_counter = 0
+    node_list = [int(x) for x in graph.nodes()]
+
+    with open(new_data / 'node_list.txt', 'w') as file:
+        file.write(str(node_list))
+
+    with open(new_data / 'mapping.txt', 'w') as file:
+        file.write(str(mapping))
+
+    with log.scope.info('Delete graph...'):
+        del graph
+
+    with log.scope.info('Collecting statistics...'):
+
+        publications = list(publications_in('COMP', between=(1990, 2018)))
+        log.info(f'{blue | len(publications)} publications.')
+
+        stats = []
+        for publication in publications:
+
+            publication_counter += 1
+
+            authors = []
+            for author in publication['authors']:
+                authors.append(author['id'])
+
+            # statistics = {}
+            statistics = {'publication_id': publication['id']}
+
+            all = True
+            for id in authors:
+                # log.info(f'id {id}, mapping[id] {mapping[id]  if id in mapping.keys() else 0}')
+                if id not in mapping.keys():
+                    all = False
+                elif mapping[id] not in node_list:
+                    all = False
+
+            if all:
+                for author in authors:
+                    if str(mapping[author]) in communities.keys():
+                        community = f'community {communities[str(mapping[author])]}'
+
+                        if community in statistics.keys():
+                            statistics[community] += 1
+                        else:
+                            statistics[community] = 1
+
+            # if publication_counter % 500 == 0:
+            #     log.info(f'{blue | publication_counter} ...')
+
+            stats.append(statistics)
+
+    with log.scope.info('Saving statistics into a file...'):
+        with open(new_data / 'stats_largest_cluster.json', 'w') as file:
+            json.dump(stats, file)
