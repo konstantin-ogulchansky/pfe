904b5625f60799b4eb19f2f92fd5a2741887f034
SofiiaShelest <soniashelest@gmail.com>
2021-04-29 00:57:35 +0200

    Update plot_heatmap.py

--- src/pfe/matrices/plot_heatmap.py
+++ src/pfe/matrices/plot_heatmap.py
@@ -12,12 +12,13 @@ log.info('Starting...')
 # data_path = Path('test-data/COMP-data')
 
 
-def plot_matrix(m: pd.DataFrame, file_name, data_path: Path, title='', subtitle='', prob=False, exclude_columns=False):
-    if len(m.columns.tolist()) <= 1:
-        return
+def plot_matrix(m: pd.DataFrame, file_name, data_path: Path, title='', subtitle='', xlabel='', ylabel='', prob=False,
+                exclude_columns=False):
+    # if len(m.columns.tolist()) <= 1:
+    #     return
 
     if exclude_columns:
-        return plot_matrix_exclude_columns_from_heatmap(m, file_name, data_path, title, subtitle, prob)
+        return plot_matrix_exclude_columns_from_heatmap(m, file_name, data_path, title, subtitle, xlabel, ylabel, prob)
 
     with log.scope.info('Plotting...'):
         plt.clf()
@@ -28,23 +29,29 @@ def plot_matrix(m: pd.DataFrame, file_name, data_path: Path, title='', subtitle=
             fig, ax = plt.subplots(figsize=(82, 82))
         elif len(m.columns.tolist()) > 45:
             fig, ax = plt.subplots(figsize=(64, 64))
-        else:
+        elif len(m.columns.tolist()) > 25:
             fig, ax = plt.subplots(figsize=(32, 32))
+        else:
+            fig, ax = plt.subplots(figsize=(16, 16))
 
         if prob:
             ax = sns.heatmap(m, ax=ax, annot=True, fmt=".4f", cmap="YlGn", linewidths=.5)
         else:
             ax = sns.heatmap(m, ax=ax, annot=True, fmt=".0f", cmap="YlGn", linewidths=.5)
 
-        fig.suptitle(title, fontsize=80)
-        ax.set_title(subtitle, fontdict=dict(ha='center', va='center', fontsize=70))
+        plt.xlabel(xlabel)
+        plt.ylabel(ylabel)
+
+        fig.suptitle(title, fontsize=40)
+        ax.set_title(subtitle, fontdict=dict(ha='center', va='center', fontsize=20))
         ax.set_aspect("equal")
 
         plt.savefig(data_path / f'{file_name}.png')
         # plt.show()
 
 
-def plot_matrix_exclude_columns_from_heatmap(m: pd.DataFrame, file_name, data_path: Path, title='', subtitle='', prob=False):
+def plot_matrix_exclude_columns_from_heatmap(m: pd.DataFrame, file_name, data_path: Path, title='', subtitle='',
+                                             xlabel='', ylabel='', prob=False):
     m = m.sort_values(by='sizes', ascending=False)
     cols = m.columns.tolist()
     all_cols = len(cols)
@@ -69,8 +76,10 @@ def plot_matrix_exclude_columns_from_heatmap(m: pd.DataFrame, file_name, data_pa
             fig, ax = plt.subplots(figsize=(100, 100))
         elif len(m.columns.tolist()) > 45:
             fig, ax = plt.subplots(figsize=(82, 82))
-        else:
+        elif len(m.columns.tolist()) > 25:
             fig, ax = plt.subplots(figsize=(64, 64))
+        else:
+            fig, ax = plt.subplots(figsize=(32, 32))
 
         if prob:
             mask = np.zeros((prob_cols, all_cols))
@@ -99,6 +108,9 @@ def plot_matrix_exclude_columns_from_heatmap(m: pd.DataFrame, file_name, data_pa
         fig.suptitle(title, fontsize=80)
         ax.set_title(subtitle, fontdict=dict(ha='center', va='center', fontsize=70))
         ax.set_aspect("equal")
+
+        plt.xlabel(xlabel)
+        plt.ylabel(ylabel)
 
         plt.savefig(data_path / f'{file_name}.png')
         # plt.show()
