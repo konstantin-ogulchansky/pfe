e968c57b344e3091f740988e67eadb78f48a3e4f
SofiiaShelest <soniashelest@gmail.com>
2021-05-30 19:09:45 +0200

    Update generate_data.py

--- src/pfe/matrices/generate_data.py
+++ src/pfe/matrices/generate_data.py
@@ -1,229 +1,24 @@
 import csv
 import json
 import os
+from shutil import copyfile
+
+import networkx as nx
+import igraph as ig
+
 from os.path import basename
 from pathlib import Path
-import igraph as ig
+
+from pfe.matrices.semiusefull_stuff import get_year_from_filename
 from pfe.misc.log import Pretty, Log
 from pfe.misc.style import blue, underlined, magenta
 from pfe.parse import publications_in
-import networkx as nx
+from pfe.tasks.communities import leiden_nx, louvain
 
 
 # data = Path('test-data/COMP-data')
 # graph_path = Path('graph/nice')
 # new_data = Path('test-data/COMP-data')
-from pfe.tasks.communities import leiden_nx, louvain
-
-
-# def create_data_igraph(graph: ig.Graph, mapping_path: Path,  new_data: Path, log=Pretty()):
-def create_data_igraph(graph: ig.Graph, new_data: Path, log=Pretty()):
-
-    # with log.scope.info('Reading `ig.Graph`...'):
-    #     # graph = ig.Graph.Read_Pajek(str(data / 'ig_full_graph_relabeled_nodes.net'))
-    #     graph = ig.Graph.Read_Pajek(str(data / graph_path / 'ig_comp_nice_all_int_graph.net'))
-    #
-    #     log.info(f'Read a graph with '
-    #              f'{blue | len(graph.vs)} vertices and '
-    #              f'{blue | len(graph.es)} edges.')
-    #
-    # graph = graph.clusters().giant()
-    #
-    # log.info(f'Subgraph with '
-    #          f'{blue | len(graph.vs)} vertices and '
-    #          f'{blue | len(graph.es)} edges.')
-
-    with log.scope.info(f'Detecting communities using the {underlined | "ig Leiden"} method...'):
-        objective = 'modularity'
-        communities = graph.community_leiden(weights='weight', objective_function=objective)
-
-        log.info('The objective:               ' + objective)
-        log.info('The number of communities:   ' + str(blue | len(communities)))
-        log.info('The modularity value:        ' + str(blue | communities.modularity))
-
-    with log.scope.info('Saving the modularity value into a file...'):
-        with open(new_data / 'ig_leiden_modularity.json', 'w') as file:
-            json.dump(communities.modularity, file)
-
-    # skip communities with 1 member
-    communities = [c for c in communities if len(c) > 1]
-
-    with log.scope.info('Saving communities into a file...'):
-        with open(new_data / 'ig_leiden_communities.json', 'w') as file:
-            json.dump(dict(enumerate(communities)), file)
-
-    return
-
-    communities = dict(enumerate(communities))
-    new_dict = {}
-
-    for k_community, v_authors in communities.items():
-        for author in v_authors:
-            new_dict[int(author)] = k_community
-
-    communities = new_dict
-
-    with log.scope.info('Saving author-community into a file...'):
-        with open(new_data / 'leiden_author-community.json', 'w') as file:
-            json.dump(communities, file)
-
-    with log.scope.info('Reading authors-communities.'):
-        with open(new_data / 'leiden_author-community.json', 'r') as file:
-            communities = json.load(file)
-
-    mapping = {}
-    # with open(data / 'nx_node_mapping.csv', 'r') as file:
-    # with open(data/ graph_path / 'nx_node_mapping_nice_all_int.csv', 'r') as file:
-    with open(mapping_path, 'r') as file:
-
-        reader = csv.reader(file)
-        for row in reader:
-            mapping[row[0]] = int(row[1])
-
-    publication_counter = 0
-    node_list = [x.index for x in graph.vs]
-
-    with open(new_data / 'node_list.txt', 'w') as file:
-        file.write(str(node_list))
-
-    with open(new_data / 'mapping.txt', 'w') as file:
-        file.write(str(mapping))
-
-    with log.scope.info('Delete graph...'):
-        del graph
-
-    with log.scope.info('Collecting statistics...'):
-
-        publications = list(publications_in('COMP', between=(1990, 2018)))
-        log.info(f'{blue | len(publications)} publications.')
-
-        stats = []
-        for publication in publications:
-
-            publication_counter += 1
-
-            authors = []
-            for author in publication['authors']:
-                authors.append(author['id'])
-
-            # statistics = {}
-            statistics = {'publication_id': publication['id']}
-
-            all = True
-            for id in authors:
-                # log.info(f'id {id}, mapping[id] {mapping[id]}')
-                if id not in mapping.keys():
-                    all = False
-                elif mapping[id] not in node_list:
-                    all = False
-
-            if all:
-                for author in authors:
-                    if str(mapping[author]) in communities.keys():
-
-                        community = f'community {communities[str(mapping[author])]}'
-
-                        if community in statistics.keys():
-                            statistics[community] += 1
-                        else:
-                            statistics[community] = 1
-
-            # if publication_counter % 500 == 0:
-            #     log.info(f'{blue | publication_counter} ...')
-
-            stats.append(statistics)
-
-    with log.scope.info('Saving statistics into a file...'):
-        with open(new_data / 'stats_largest_cluster.json', 'w') as file:
-            json.dump(stats, file)
-
-
-def create_data_cdlib_relabeled_graph(graph: nx.Graph,  mapping_path: Path,  new_data: Path, publications_till=2018, log_file=''):
-    log: Log = Pretty()
-
-    leiden_nx(graph, new_data, log)
-
-    log.info('Reading communities.')
-    with open(new_data / 'leiden_communities.json', 'r') as file:
-            communities = json.load(file)
-
-    new_dict = {}
-
-    for k_community, v_authors in communities.items():
-        for author in v_authors:
-            new_dict[str(author)] = int(k_community)
-
-    communities = new_dict
-
-    log.info('Saving author-community into a file...')
-    with open(new_data / 'leiden_author-community.json', 'w') as file:
-        json.dump(communities, file)
-
-    mapping = {}
-    with open(mapping_path, 'r') as file:
-        reader = csv.reader(file)
-        for row in reader:
-            mapping[row[0]] = int(row[1])
-
-    publication_counter = 0
-    node_list = [int(x) for x in graph.nodes()]
-
-    with open(new_data / 'node_list.txt', 'w') as file:
-        file.write(str(node_list))
-
-    with open(new_data / 'mapping.txt', 'w') as file:
-        file.write(str(mapping))
-
-    with log.scope.info('Delete graph...'):
-        del graph
-
-    with log.scope.info('Collecting statistics...'):
-
-        publications = list(publications_in('COMP', between=(1990, publications_till)))
-        log.info(f'{blue | len(publications)} publications.')
-
-        if log_file != '':
-            with open(new_data / log_file, 'a+') as file:
-                file.write(f'{len(publications)} publications.')
-
-        stats = []
-        for publication in publications:
-
-            publication_counter += 1
-
-            authors = []
-            for author in publication['authors']:
-                authors.append(author['id'])
-
-            # statistics = {}
-            statistics = {'publication_id': publication['id']}
-
-            all = True
-            for id in authors:
-                # log.info(f'id {id}, mapping[id] {mapping[id]  if id in mapping.keys() else 0}')
-                if id not in mapping.keys():
-                    all = False
-                elif mapping[id] not in node_list:
-                    all = False
-
-            if all:
-                for author in authors:
-                    if str(mapping[author]) in communities.keys():
-                        community = f'community {communities[str(mapping[author])]}'
-
-                        if community in statistics.keys():
-                            statistics[community] += 1
-                        else:
-                            statistics[community] = 1
-
-            # if publication_counter % 500 == 0:
-            #     log.info(f'{blue | publication_counter} ...')
-
-            stats.append(statistics)
-
-    log.info('Saving statistics into a file...')
-    with open(new_data / 'stats_largest_cluster.json', 'w') as file:
-        json.dump(stats, file)
 
 
 def create_data(graph: nx.Graph, new_data: Path, algorithm: str, publications_till=2018, log_file=Path(''), log=Pretty()):
@@ -310,45 +105,75 @@ def create_data(graph: nx.Graph, new_data: Path, algorithm: str, publications_ti
         json.dump(stats, file)
 
 
-def gererate_ig_leiden_data(log=Pretty()):
+def generate_data_for_algorithms(graph_folders):
+    # root_path = Path('test-data/COMP-data/graph/nice')
+    # graph_folders = [
+    #     # all_years_f := root_path/Path('all_years/float'),
+    #     # all_years_i := root_path/Path('all_years/int'),
+    #     by_year_i := root_path/Path('by_year/int'),
+    #     # by_year_f := root_path/Path('by_year/float'),
+    #     # by_year_fs := root_path/Path('by_year/float_selfloop'),
+    # ]
+
+    log = Pretty()
+    # algorithms = ['leiden', 'louvain']
+    algorithms = ['louvain']
     log_file_name = Path('graph.log')
-    root_path = Path('test-data/COMP-data/graph/nice')
-    folders = [
-        # all_years_f := root_path/Path('all_years/float'),
-        # all_years_i := root_path/Path('all_years/int'),
-        by_year_i := root_path/Path('by_year/int'),
-        by_year_f := root_path / Path('by_year/float'),
-        # by_year_fs := root_path/Path('by_year/float_selfloop'),
-    ]
 
-    for graph_folder in folders:
-        for subfolder in graph_folder.iterdir():
-            if os.path.isdir(subfolder):
-                for graph_file in subfolder.iterdir():
-                    if graph_file.suffix == '.net':
-                        with log.scope.info(f'Reading `ig.Graph`...{magenta | basename(graph_file)}'):
-                            graph = ig.Graph.Read_Pajek(str(graph_file))
+    for algorithm in algorithms:
+        for graph_folder in graph_folders:
+            for graph_file in graph_folder.iterdir():
 
-                            before = f'Read a graph with ' \
-                                     f'{blue | len(graph.vs)} vertices and ' \
-                                     f'{blue | len(graph.es)} edges.'
-                            log.info(before)
+                # GraphML
+                if graph_file.suffix == '.xml':
+                    common_filename = splitext(basename(graph_file))[0]
 
-                            graph = graph.clusters().giant()
+                    with log.scope.info(f'Creating a folder...'
+                                        f'{magenta | common_filename}'):
+                        new_subdirectory = graph_folder / common_filename
 
-                            with open(subfolder / log_file_name, 'a+') as log_file:
-                                log_file.write(f'igraph Leiden: Graph: '
-                                               f'{len(graph.vs)} nodes '
-                                               f'and {len(graph.es)} edges.\n')
+                        if not os.path.exists(new_subdirectory):
+                            new_subdirectory.mkdir()
 
-                            after = f'Subgraph with ' \
-                                    f'{blue | len(graph.vs)} vertices and ' \
-                                    f'{blue | len(graph.es)} edges.'
-                            log.info(after)
+                    with log.scope.info(f'Reading `GraphML`... {magenta | basename(graph_file).replace("_", " ")}'):
+                        graph = nx.read_graphml(graph_file)
 
-                            with open(subfolder / log_file_name, 'a+') as log_file:
-                                log_file.write(f'igraph Leiden: Subgraph, largest component: '
-                                               f'{len(graph.vs)} nodes '
-                                               f'and {len(graph.es)} edges.\n')
+                        log.info(f'Read a graph with '
+                                 f'{blue | graph.number_of_nodes()} nodes and '
+                                 f'{blue | graph.number_of_edges()} edges.')
 
-                            create_data_igraph(graph, subfolder)
+                        with open(new_subdirectory / log_file_name, 'a+') as log_file:
+                            log_file.write(f'{algorithm.capitalize()}: Full graph: {graph.number_of_nodes()} nodes '
+                                           f'and {graph.number_of_edges()} edges.\n')
+
+                        largest_component = max(nx.connected_components(graph), key=len)
+                        graph = nx.subgraph(graph, largest_component)
+
+                        log.info(f'Subgraph of largest connected component '
+                                f'{blue | graph.number_of_nodes()} nodes and '
+                                f'{blue | graph.number_of_edges()} edges.')
+
+                        with open(new_subdirectory / log_file_name, 'a+') as log_file:
+                            log_file.write(f'{algorithm.capitalize()}: Subgraph, largest component: '
+                                           f'{graph.number_of_nodes()} nodes '
+                                           f'and {graph.number_of_edges()} edges.\n')
+
+                        with log.scope.info('Generating data...'):
+                            year = get_year_from_filename(graph_file)
+
+                            create_data(graph, new_subdirectory, algorithm, year, log_file_name)
+
+                        with log.scope.info('Assign community to a node and update the graph..'):
+                            copyfile(graph_file, new_subdirectory/basename(graph_file))
+                            graph = nx.read_graphml(new_subdirectory/basename(graph_file))
+
+                            with open(new_subdirectory / f'{algorithm}_author-community.json', 'r') as file:
+                                author_community = dict(json.load(file))
+
+                            # graph.remove_nodes_from([node for node in graph.nodes() if node not in author_community.keys()])
+
+                            for author in author_community.keys():
+                                graph.nodes[author][f'{algorithm}_community'] = int(author_community[author])
+
+                            nx.write_graphml(graph, new_subdirectory/basename(graph_file))
+
